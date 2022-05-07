8854351a041b68eb387b2db27a1253169835f1f3
FuenSansano <i.fuensanosano@gmail.com>
2022-05-08 00:38:37 +0200

    refactor: year_divisible_by_four_is_a_leap_year

--- java/src/test/java/LeapYearTest.java
+++ java/src/test/java/LeapYearTest.java
@@ -1,28 +1,26 @@
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class LeapYearTest {
+  @ParameterizedTest
+  @CsvSource({
+    "4",
+    "8",
+    "12"
+  })
+  public void year_divisible_by_four_is_a_leap_year(int year) {
+    assertTrue(LeapYear.isLeapYear(year));
+  }
+
 
 	@Test
 	public void year_one_is_not_a_leap_year() {
 		assertFalse(LeapYear.isLeapYear(1));
 	}
 
-  @Test
-  public void year_four_is_a_leap_year() {
-    assertTrue(LeapYear.isLeapYear(4));
-  }
-
-  @Test
-  public void year_eight_is_a_leap_year() {
-    assertTrue(LeapYear.isLeapYear(8));
-  }
-
-  @Test
-  public void year_twelve_is_a_leap_year() {
-    assertTrue(LeapYear.isLeapYear(12));
-  }
 
 }
