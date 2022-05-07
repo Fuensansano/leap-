149b4918e6f1dce72c1339809cbbb2e91ec532be
FuenSansano <i.fuensanosano@gmail.com>
2022-05-08 00:14:19 +0200

    making fail test year_one_is_not_a_leap_year

--- java/src/test/java/LeapYearTest.java
+++ java/src/test/java/LeapYearTest.java
@@ -1,12 +1,13 @@
 import org.junit.jupiter.api.Test;
 
+import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class LeapYearTest {
 
 	@Test
-	public void renameMe() {
-		assertTrue(true);
+	public void year_one_is_not_a_leap_year() {
+		assertFalse(LeapYear.isLeapYear(1));
 	}
 
 }
