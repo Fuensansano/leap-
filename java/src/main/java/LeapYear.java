db131619aab2452f1438e307f27c9c724d5d9df1
Emmanuel Valverde Ramos <evrtrabajo@gmail.com>
2022-05-08 00:54:56 +0200

    making the year_three_hundred_is_not_a_leap_year use case pass

--- java/src/main/java/LeapYear.java
+++ java/src/main/java/LeapYear.java
@@ -1,7 +1,7 @@
 public class LeapYear {
 
   public static boolean isLeapYear(int year) {
-    if (year % 4 == 0 && year != 100 && year != 200) {
+    if (year % 4 == 0 && year != 100 && year != 200 && year != 300) {
       return true;
     }
     return false;
