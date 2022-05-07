9c42a969475ebe4c8ffec310b55c61e6a90f1ee2
FuenSansano <i.fuensanosano@gmail.com>
2022-05-08 01:02:46 +0200

    making pass year_divisible_by_four_hundred_is_a_leap_year use case

--- java/src/main/java/LeapYear.java
+++ java/src/main/java/LeapYear.java
@@ -1,7 +1,7 @@
 public class LeapYear {
 
   public static boolean isLeapYear(int year) {
-    if (year % 4 == 0 && year % 100 != 0) {
+    if (year % 4 == 0 && year % 100 != 0 || year == 400) {
       return true;
     }
     return false;
